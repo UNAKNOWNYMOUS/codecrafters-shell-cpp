0597fa084acb6d47e70b202ce28873e9b1fdbc3b
UNAKNOWNYMOUS <UNAKNOWNYMOUS@protonmail.com>
2026-05-13 00:41:23 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <set>
 #include <string>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  std::set<std::string> valid_commands{"exit", "echo", "type"};
 
   while (true) {
     // Print shell prompt
@@ -18,6 +21,12 @@ int main() {
       break;
     } else if (command.substr(0, 5) == "echo ") {
       std::cout << command.substr(5) << '\n';
+    } else if (command.substr(0, 5) == "type ") {
+      if (valid_commands.contains(command.substr(5))) {
+        std::cout << command.substr(5) << "is a shell builtin\n";
+      } else {
+        std::cout << command << ": command not found\n";
+      }
     } else {
       std::cout << command << ": command not found\n";
     }
