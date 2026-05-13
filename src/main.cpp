22d35948102970ca1e9711d5299f7ed7199376ce
UNAKNOWNYMOUS <UNAKNOWNYMOUS@protonmail.com>
2026-05-13 00:49:25 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,11 @@ int main() {
     } else if (command.substr(0, 5) == "echo ") {
       std::cout << command.substr(5) << '\n';
     } else if (command.substr(0, 5) == "type ") {
-      if (valid_commands.contains(command.substr(5))) {
-        std::cout << command.substr(5) << " is a shell builtin\n";
+      std::string arguments{command.substr(5)};
+      if (valid_commands.contains(arguments)) {
+        std::cout << arguments << " is a shell builtin\n";
       } else {
-        std::cout << command.substr(5) << ": not found\n";
+        std::cout << arguments << ": not found\n";
       }
     } else {
       std::cout << command << ": command not found\n";
